8202807041a68718bbc8c8c84fbd569bfd99e41b
Oleg Abelev <oleg_80@list.ru>
2020-04-10 17:37:53 +0300

    ДЗ№3

---
+++ Main.java
@@ -0,0 +1,85 @@
+package hw3.io;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // пункт 1
+        try {
+            byte[] arr = Files.readAllBytes(Paths.get("123/task1.txt"));
+
+            for (byte o : arr)
+                System.out.print(o);
+            System.out.println();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        // пункт 2
+        try (FileOutputStream fos = new FileOutputStream("123/summ.txt")) {
+            ArrayList<InputStream> al = new ArrayList<>();
+            al.add(new FileInputStream("123/1.txt"));
+            al.add(new FileInputStream("123/2.txt"));
+            al.add(new FileInputStream("123/3.txt"));
+            al.add(new FileInputStream("123/4.txt"));
+            al.add(new FileInputStream("123/5.txt"));
+
+            Enumeration<InputStream> e = Collections.enumeration(al);
+            SequenceInputStream in = new SequenceInputStream(e);
+
+            int x;
+
+            while ((x = in.read()) != -1) {
+                System.out.print((char) x);
+                fos.write(x);
+            }
+
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // пункт 3
+        readPage();
+    }
+
+    static void readPage() {
+        // не очень понятно, что значит "контролируем время выполнения". Сделал контроль в виде засечки времени
+        // загрузки и чтения страницы и вывода этих величин в консоль
+         long startApp = System.nanoTime();
+         try (RandomAccessFile raf = new RandomAccessFile("123/task3.txt", "r");
+                     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));) {
+             long pagesCount = raf.length() / 1800;
+             byte[] b = new byte[1800];
+
+             System.out.println("\nКоличество страниц: " + pagesCount);
+             System.out.println("Время загрузки приложения: " + ((System.nanoTime() - startApp) / 1000000) + "мc");
+
+             while (true) {
+                 System.out.println("\nВведите номер страницы от 0 до " + pagesCount + " или -1 для выхода.");
+                 long numberPage = Long.parseLong(br.readLine());
+                 long startRead = System.nanoTime();
+
+                 if (numberPage <= pagesCount && numberPage >= 0) {
+                     raf.seek(numberPage * 1800);
+                     raf.read(b, 0, b.length);
+                     for (byte page : b) System.out.print((char) page);
+                     System.out.println("\nВремя чтения страницы: " + ((System.nanoTime() - startRead) / 1000000) + "мc");
+                 } else if (numberPage == -1) {
+                     System.exit(0);
+                 } else {
+                     System.out.println("Неправильный номер страницы: " + numberPage);
+                 }
+             }
+
+         } catch(Exception e){
+             e.printStackTrace();
+         }
+    }
+}
